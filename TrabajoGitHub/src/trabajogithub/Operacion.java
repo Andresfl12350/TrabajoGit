4f02170e673e40ebf6e3595f66a5c4c6922b9f7a
Andres <andres-franciscog@hotmail.com>
2020-11-19 00:48:31 -0500

    Creacion_metodo_resta

--- TrabajoGitHub/src/trabajogithub/Operacion.java
+++ TrabajoGitHub/src/trabajogithub/Operacion.java
@@ -17,5 +17,9 @@ public class Operacion {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    public int Resta(int a, int b){
+        
+        return a-b;
+    }
     
 }
